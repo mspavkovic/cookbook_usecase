4cac20487acdcb73289b25af4699f1b929c343c1
mspavkovic <34711927+mspavkovic@users.noreply.github.com>
2022-06-14 16:10:26 +0200

    Develop (#2)
    * Model improvements, service & mappers refactor

--- src/main/java/com/valcon/cookbook/web/dto/RecipeDto.java
+++ src/main/java/com/valcon/cookbook/web/dto/RecipeDto.java
@@ -6,8 +6,6 @@ import java.util.Set;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-import com.valcon.cookbook.domain.ingredient.Ingredient;
-
 import lombok.Builder;
 
 public record RecipeDto(@NotNull String name,
